e28bf267df80f1185bcad16242d1238621324bfa
mateussantosrodrigues650-hash <leozinnxd00@gmail.com>
2026-07-08 17:01:31 -0300

    update_projeto
    estou aprendendo a criar janela em TKINTER e completar o codigo

---
+++ pensamento_projeto_leonardoSales.py
@@ -0,0 +1,86 @@
+
+import tkinter as tk
+from tkinter import messagebox, ttk
+'''
+
+
+>>projeto barbearia:
+
+>PO (Como dono do negócio: Quero um sistema de cortes para minha barbearia,
+para que eu possa controlar quais cortes e agendamentos.)
+
+>QA (Como cliente: Quero um sistema de cortes para minha barbearia, 
+para que eu possa escolher meus cortes de forma mais fácil e rápida.)
+
+>Tech (Como programador: Quero um sistema inteligente de cortes para minha barbearia, 
+para que eu possa desenvolver um software de mais qualidade e eficiência para o negócio.)
+
+>Dev (como programador: Quero um sistema de cortes para minha barbearia, 
+para que eu possa aplicar funcionalidades interessantes para atender
+ necessidades do negócio e do cliente.)
+
+>UX (Como designer de exxperiência do usuário: quero um sistema de cortes para minha barbearia, 
+para que eu possa criar uma interface limpa e flúida para os usuários, garantindo 
+uma experiência satisfatória.)
+
+>IA (Como analista de dados: Quero um sistema de cortes para minha barbearia,
+ para que eu possa identificar padrões, criar algoritimos de consumo e 
+ recomendações em Marketing.)
+
+
+ '''
+
+print('-' * 48 + '\n')
+print("Bem-vindo ao sistema de menu de cortes!","\n") 
+
+print("Esses são todas as opções: \n")
+
+print("1 - Cadastrar corte\n")
+print("2 - Listar cortes\n")
+print("3 - realizar agendamento\n")
+print("0 - Sair do sistema\n")
+ 
+print('-' * 48 + '\n')
+
+opcao = int(input('escolha sua opção: '))
+
+if opcao == 1:
+    print("cadastrando cortes...")
+
+    if c1_nome == "1":
+        c1_nome = input("digite o nome do corte: ")
+        c1_estoque = int(input("coloque a quantidade em estoque: "))
+        c1_preco = float(input("coloque o preço do corte: "))
+        c1_descricao = input("descreva o corte: ")
+        print(f"✂corte {c1_nome} cadastrado com sucesso na lista 1!✂")
+ 
+    elif c2_nome == "1":
+        c2_nome = input("digite o nome do corte: ")
+        c2_estoque = int(input("coloque a quantidade em estoque: "))
+        c2_preco = float(input("coloque o preço do corte: "))
+        c2_descricao = input("descreva o corte: ")
+        print(f"✂corte {c2_nome} cadastrado com sucesso na lista 2!✂")
+
+    elif c3_nome == "1":
+        c3_nome = input("digite o nome do corte: ")
+        c3_estoque = int(input("coloque a quantidade em estoque: "))
+        c3_preco = float(input("coloque o preço do corte: "))
+        c3_descricao = input("descreva o corte: ")
+        print(f"✂corte {c3_nome} cadastrado com sucesso na lista 3!✂")
+
+    elif c4_nome == "1":
+        c4_nome = input("digite o nome do corte: ")
+        c4_estoque = int(input("coloque a quantidade em estoque: "))
+        c4_preco = float(input("coloque o preço do corte: "))
+        c4_descricao = input("descreva o corte: ")
+        print(f"✂corte {c4_nome} cadastrado com sucesso na lista 4!✂")
+
+    elif c5_nome == "1":
+        c5_nome = input("digite o nome do corte: ")
+        c5_estoque = int(input("coloque a quantidade em estoque: "))
+        c5_preco = float(input("coloque o preço do corte: "))
+        c5_descricao = input("descreva o corte: ")
+        print(f"✂corte {c5_nome} cadastrado com sucesso na lista 5!✂")
+
+    else: 
+        print("opção inválida, tente novamente!")
